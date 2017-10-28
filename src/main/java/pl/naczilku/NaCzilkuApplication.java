3b2a553087660e71d5429adff51084e476211b97
pafker <ppatrix555@gmail.com>
2017-10-28 22:41:21 +0200

    Spring Boot initializer class

---
+++ src/main/java/pl/naczilku/NaCzilkuApplication.java
@@ -0,0 +1,12 @@
+package pl.naczilku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaCzilkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaCzilkuApplication.class, args);
+	}
+}
