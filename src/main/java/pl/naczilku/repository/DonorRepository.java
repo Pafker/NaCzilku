b717a03fd59837fd8cc3e4ae976af33934d738de
pafker <ppatrix555@gmail.com>
2017-11-03 10:12:40 +0100

    Fixed interface

--- src/main/java/pl/naczilku/repository/DonorRepository.java
+++ src/main/java/pl/naczilku/repository/DonorRepository.java
@@ -1,6 +1,7 @@
 package pl.naczilku.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pl.naczilku.model.Donor;
 
